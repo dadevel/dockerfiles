92d8bc76b231e4e6a94093b23f57a75e96e2a5b5
dadevel <dadevel@disroot.org>
2020-07-28 08:51:09 +0200

    hostapd: add option to set transmission power limit

--- hostapd/entrypoint.sh
+++ hostapd/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+[ -z "$HOSTAPD_TRANSMISSION_POWER_LIMIT" ] || iw "${HOSTAPD_INTERFACE:-phy0}" set txpower limit "$HOSTAPD_TRANSMISSION_POWER_LIMIT"
 cat /app/config/*.conf > /dev/shm/hostapd.conf && \
     exec hostapd "$@" /dev/shm/hostapd.conf
 
