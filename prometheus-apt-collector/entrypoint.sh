a3f20f1ca5de3858bce30be7d12290ae73322046
Daniel <dadevel@disroot.org>
2020-11-17 10:54:12 +0100

    replace openssl with libressl and add health checks

--- prometheus-apt-collector/entrypoint.sh
+++ prometheus-apt-collector/entrypoint.sh
@@ -4,9 +4,13 @@ TEXTFILE_DIR="${APT_COLLECTOR_TEXTFILE_DIR:-/app/data}"
 INTERVAL="${APT_COLLECTOR_INTERVAL:-3600}"
 
 while :; do
-    apt.sh > "$TEXTFILE_DIR/apt.prom.$$" && \
-        mv "$TEXTFILE_DIR/apt.prom.$$" "$TEXTFILE_DIR/apt.prom" || \
+    if apt.sh > "$TEXTFILE_DIR/apt.prom.$$"; then
+        mv "$TEXTFILE_DIR/apt.prom.$$" "$TEXTFILE_DIR/apt.prom"
+        touch /dev/shm/health
+    else
         echo "error: metric collection failed with exit code $?"
+        rm -f /dev/shm/health
+    fi
     sleep "$INTERVAL"
 done
 
--- prometheus-smart-collector/entrypoint.sh
+++ prometheus-smart-collector/entrypoint.sh
@@ -4,9 +4,13 @@ TEXTFILE_DIR="${SMARTMON_COLLECTOR_TEXTFILE_DIR:-/app/data}"
 INTERVAL="${SMARTMON_COLLECTOR_INTERVAL:-3600}"
 
 while :; do
-    smartmon.py > "$TEXTFILE_DIR/smartmon.prom.$$" && \
-        mv "$TEXTFILE_DIR/smartmon.prom.$$" "$TEXTFILE_DIR/smartmon.prom" || \
+    if smartmon.py > "$TEXTFILE_DIR/smartmon.prom.$$"; then
+        mv "$TEXTFILE_DIR/smartmon.prom.$$" "$TEXTFILE_DIR/smartmon.prom"
+        touch /dev/shm/health
+    else
         echo "error: metric collection failed with exit code $?"
+        rm -f /dev/shm/health
+    fi
     sleep "$INTERVAL"
 done
 
