6604cb951d489c6d8442cf7f7dc7fe2e7a2a303f
dadevel <dadevel@disroot.org>
2021-06-11 12:39:17 +0200

    grafana: fix plugin installation

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -4,10 +4,8 @@ for plugin in $GRAFANA_PLUGINS; do
         echo "installing plugin ${plugin}" && \
             wget -q -O "/app/data/plugins/${plugin}.zip" "https://grafana.com/api/plugins/${plugin}/versions/latest/download" && \
             unzip -q -d /app/data/plugins/ "/app/data/plugins/${plugin}.zip" && \
-            mv "/app/data/plugins/${plugin}"-*/ "/app/data/plugins/${plugin}/" && \
             rm "/app/data/plugins/${plugin}.zip" || \
             echo "failed to install plugin ${plugin}" >&2
     fi
 done
 exec grafana-server "$@"
-
