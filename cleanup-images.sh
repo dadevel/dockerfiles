f9750112a339c2d92c518a1bcf7f18b816f9c997
dadevel <dadevel@disroot.org>
2022-04-29 08:55:14 +0200

    ci: delete untagged images

---
+++ cleanup-images.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+github() {
+  echo "$@" >&2
+  curl -sSf -H 'Accept: application/vnd.github.v3+json' -u "$GITHUB_USER:$GITHUB_TOKEN" "$@"
+}
+
+github_paged() {
+    for (( i=1; 1; i++ )); do
+        response="$(github "$1&page=${i}&per_page=10")"
+        "$(jq -r 'length > 0' <<< "${response}")" || break
+        jq -r "$2" <<< "${response}"
+    done
+}
+
+github_paged 'https://api.github.com/user/packages?package_type=container' '.[].url' | while read -r package_url; do
+    github_paged "${package_url}/versions?package_type=container" '.[]|select(.metadata.container.tags | length == 0)|.url' | while read -r version_url; do
+        github "${version_url}" -X DELETE 
+    done
+done
