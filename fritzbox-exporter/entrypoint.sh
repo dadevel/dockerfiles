69fd5dfb4aa23a269ccf040920c28b126cb1a72a
dadevel <dadevel@disroot.org>
2022-08-29 22:15:04 +0200

    drop fritzbox-exporter

--- fritzbox-exporter/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -eu
-export PASSWORD="$FRITZBOX_PASSWORD"
-exec fritzbox-exporter -gateway-url "$FRITZBOX_API_URL" -gateway-luaurl "$FRITZBOX_WEB_URL" -username "$FRITZBOX_USERNAME" "$@"
